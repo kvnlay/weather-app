ab3f20f4ca5953c6ff0ad0182792e2094482eac9
kvnlay <kingstevens007@gmail.com>
2019-07-19 16:25:14 +0100

    Add html and css styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import './css/reset.css';
 import './css/styles.css';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
